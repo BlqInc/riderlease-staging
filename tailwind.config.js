1da142d9751ff3e9fd81be72c0665fa663756ec8
Sol Hong <blqinc0901@gmail.com>
2025-11-06 21:23:51 +0900

    feat: 전체 로직 구현 및 기능 고도화

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./*.{ts,tsx}",
+    "./{components,lib,data}/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {},
